120e5c0de2feff990f0cb2f03ad16f268210aac2
julpod <julian.montagna@awkbit.com>
2025-04-10 00:23:49 +0000

    updated devcontainer

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -5,7 +5,14 @@ set -e
 [ -s "/opt/.ssh" ] && cp -r /opt/.ssh ~/
 [ -s "/workspace/.devcontainer/etc/.zshrc" ] && cp -r /workspace/.devcontainer/etc/.zshrc ~/.zshrc
 
+mkdir -p ~/.mc && cp -a /workspace/.devcontainer/etc/mc/* ~/.mc
+# If the "myapp-bucket" minio bucket is already created, this will skip it.
+mc mb local/myapp-bucket || true
+
 # Solve `detected dubious ownership in repository`
 git config --global --add safe.directory /var/www
+
+# Initialize pm2 apps
+# pm2 start apps.config.yaml
 
 exec "$@"
